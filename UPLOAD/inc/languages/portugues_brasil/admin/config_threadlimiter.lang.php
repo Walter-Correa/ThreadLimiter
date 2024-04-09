9ab1ccc8fdeb7009662d6593cd7aa355a247f6d0
Walter-Correa <walt.correa@gmail.com>
2024-04-09 15:22:03 +0100

    Create config_threadlimiter.lang.php

---
+++ UPLOAD/inc/languages/portugues_brasil/admin/config_threadlimiter.lang.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* ThreadLimiter - 1.2 Config Language File Portuguese Brazil
+* Licensed under GNU/GPL v3
+*/
+
+$l['threadlimiter'] = "ThreadLimiter";
+$l['threadlimiter_desc'] = "Impede que os usuários postem mais do que a contagem definida de tópicos em determinados fóruns.";
+$l['threadlimiter_settings_title'] = "Configurações de ThreadLimiter";
+$l['threadlimiter_settings_title_desc'] = "Configurações do plugin ThreadLimiter";
+$l['threadlimiter_enable_title'] = "Ativar o plugin ThreadLimiter?";
+$l['threadlimiter_enable_title_desc'] = "Marque Sim para ativar!";
+$l['threadlimiter_gid_title'] = "Grupos de Usuários";
+$l['threadlimiter_gid_title_desc'] = "Determine os grupos de usuários aos quais este regulamento se aplica. (segure Shift/Strg para selecionar mais de um)";
+$l['threadlimiter_limit_title'] = "Máximo de tópicos em fóruns definidos.";
+$l['threadlimiter_limit_title_desc'] = "Insira o número de tópicos que os usuários do(s) grupo(s) selecionado(s) podem criar nos fóruns definidos. (padrão: 2)";
+$l['threadlimiter_reset_time_title'] = "Resetar o limite de tópicos após X dias";
+$l['threadlimiter_reset_time_title_desc'] = "Insira o número de dias até que o limite seja resetado. 0 desativa esta função.";
+$l['threadlimiter_fid_title'] = "Selecionar Fórum";
+$l['threadlimiter_fid_title_desc'] = "Selecione os fóruns nos quais as funções do plugin estão ativas. (segure Shift/Strg para selecionar mais de um - selecione apenas fóruns, sem categorias)";
+?>
