1e249fafc01ff96ccfafd6375386321f279b6d5e
Walter-Correa <walt.correa@gmail.com>
2024-04-09 15:09:21 +0100

    Create language portugues_brasil.php

---
+++ UPLOAD/inc/languages/portugues_brasil/portugues_brasil.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* ThreadLimiter - 1.0 Language File Portuguese Brazil
+* Licensed under GNU/GPL v3
+*/
+
+$l['threadlimiter_error_one'] = "Desculpe - Você não pode postar mais de {1} tópico neste fórum!";
+$l['threadlimiter_error_more'] = "Desculpe - Você não pode postar mais de {1} tópicos neste fórum!";
+?>
